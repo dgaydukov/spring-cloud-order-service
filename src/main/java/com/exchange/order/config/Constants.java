fcb59925e96e4515e6fae0a67babf50f22fc60a2
Dmitriy Gaydukov <gaydukov89@gmail.com>
2024-05-21 11:18:02 +0400

    add language header forwarding

--- src/main/java/com/exchange/order/config/Constants.java
+++ src/main/java/com/exchange/order/config/Constants.java
@@ -2,4 +2,5 @@ package com.exchange.order.config;
 
 public interface Constants {
     Object[] EMPTY_PARAMS = new Object[]{};
+    String HEADER_ACCEPT_LANGUAGE = "Accept-Language";
 }
---
+++ src/main/java/com/exchange/order/config/feign/CustomRequestInterceptor.java
@@ -0,0 +1,28 @@
+package com.exchange.order.config.feign;
+
+import com.exchange.order.config.Constants;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+class CustomRequestInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes instanceof ServletRequestAttributes) {
+            HttpServletRequest webRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+            webRequest.getHeaderNames()
+                    .asIterator()
+                    .forEachRemaining(h -> {
+                        if (Constants.HEADER_ACCEPT_LANGUAGE.equalsIgnoreCase(h)) {
+                            template.header(h, webRequest.getHeader(h));
+                        }
+                    });
+        }
+    }
+}
