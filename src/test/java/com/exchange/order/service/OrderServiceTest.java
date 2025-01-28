faa1ffa1fd4dae78ec7f3bd3d4631e8c9c7b8bce
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-01-28 21:20:29 +0300

    add order service test

--- src/test/java/com/exchange/order/service/OrderServiceTest.java
+++ src/test/java/com/exchange/order/service/OrderServiceTest.java
@@ -1,12 +1,42 @@
 package com.exchange.order.service;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.exchange.order.domain.Asset;
+import com.exchange.order.domain.ConvertOrder;
+import com.exchange.order.exception.AppException;
+import com.exchange.order.facade.AssetFacade;
+import com.exchange.order.service.impl.OrderServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class OrderServiceTest {
+    private static final String SYMBOL = "BTC";
 
-    private OrderService orderService;
+    @Test
+    public void getOrderTest(){
+        MessageTranslationService messageTranslationService = Mockito.mock(MessageTranslationService.class);
+        AssetFacade assetFacade = Mockito.mock(AssetFacade.class);
+        OrderService orderService = new OrderServiceImpl(messageTranslationService, assetFacade);
+        AppException orderNotFound = Assertions.assertThrows(AppException.class,
+                () -> orderService.getOrder(SYMBOL), "Exception should be thrown");
+        Assertions.assertEquals(orderNotFound.getMessage(), "order_not_found");
 
-    @BeforeEach
-    public void init() {
+        // adding actual order
+        ConvertOrder order = new ConvertOrder();
+        order.setSymbol(SYMBOL);
+        order.setPrice(100);
+        order.setQuantity(1);
+        orderService.addOrder(order);
+
+        // check that order can be fetched
+        Asset asset = new Asset();
+        asset.setSymbol(SYMBOL);
+        asset.setPrice(100);
+        Mockito.when(assetFacade.getAsset(SYMBOL)).thenReturn(asset);
+        ConvertOrder fetchedOrder = orderService.getOrder(SYMBOL);
+        Assertions.assertEquals(SYMBOL, fetchedOrder.getSymbol(), "symbol mismatch");
+        Assertions.assertEquals(100, fetchedOrder.getPrice(), "price mismatch");
+        Assertions.assertEquals(1, fetchedOrder.getQuantity(), "quantity mismatch");
+        Assertions.assertEquals(100, fetchedOrder.getAmount(), "amount mismatch");
     }
 }
